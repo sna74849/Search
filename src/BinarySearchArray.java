93238be29bb7af2d1639eb968433f04252fb6570
sna74849 <t.munekage@gmail.com>
2020-03-20 14:55:55 +0900

    modify

--- src/BinarySearchArray.java
+++ src/BinarySearchArray.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * @author http://software-engineering-lab.com/
  *
--- src/BinarySearchList.java
+++ src/BinarySearchList.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Comparator;
 /**
  * @author http://software-engineering-lab.com/
  *
